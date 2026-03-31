0d165850bb4a990bb7251fab2faebe92f4b2f0f6
Rallapalli Goutham Deekshith <rgdeekshith@gmail.com>
2026-03-31 22:59:59 +0530

    Update loading_bronze_data.sql

--- automation/loading_bronze_data.sql
+++ automation/loading_bronze_data.sql
@@ -1,52 +1,78 @@
+-- This procedure centralizes the data ingestion for the entire Bronze layer.
+-- It accepts @BasePath, which is the folder path where the 'datasets' folder is located.
 CREATE OR ALTER PROCEDURE bronze.load_bronze (@BasePath NVARCHAR(MAX)) AS
 BEGIN
-	DECLARE @DynamicSQL NVARCHAR(MAX);
+    -- We use a variable to build our SQL commands as text before executing them.
+    DECLARE @DynamicSQL NVARCHAR(MAX);
 
-	-- FOR bronze.crm_cust_info
-	TRUNCATE table bronze.crm_cust_info; 
-	SET @DynamicSQL = '
-	BULK INSERT bronze.crm_cust_info
-	FROM ''' + @BasePath + '\source_crm\cust_info.csv''
-	WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
-	EXEC sp_executesql @DynamicSQL;
+    ---------------------------------------------------------------------------
+    -- 1. Load crm_cust_info
+    ---------------------------------------------------------------------------
+    -- First, we clear existing data to prevent duplicates (Full Load strategy).
+    TRUNCATE TABLE bronze.crm_cust_info; 
 
-	-- FOR bronze.crm_prd_info
-	TRUNCATE table bronze.crm_prd_info;
-	SET @DynamicSQL = '
-	BULK INSERT bronze.crm_prd_info
-	FROM ''' + @BasePath + '\source_crm\prd_info.csv''
-	WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
-	EXEC sp_executesql @DynamicSQL;
+    -- We construct the BULK INSERT command by gluing the base path to the file sub-folder.
+    -- Note: Triple single quotes (''') are used to put one literal quote around the path.
+    SET @DynamicSQL = '
+    BULK INSERT bronze.crm_cust_info
+    FROM ''' + @BasePath + '\source_crm\cust_info.csv''
+    WITH ( 
+        FIRSTROW = 2,           -- Skip the header row in the CSV
+        FIELDTERMINATOR = '','', -- Use a comma to separate columns
+        TABLOCK                 -- Lock the table during load for better speed
+    );';
+    
+    -- This command tells SQL Server to run the text string we just built.
+    EXEC sp_executesql @DynamicSQL;
 
-	-- For bronze.crm_sales_details
-	TRUNCATE table bronze.crm_sales_details;
-	SET @DynamicSQL = '
-	BULK INSERT bronze.crm_sales_details
-	FROM ''' + @BasePath + '\source_crm\sales_details.csv''
-	WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
-	EXEC sp_executesql @DynamicSQL;
+    ---------------------------------------------------------------------------
+    -- 2. Load crm_prd_info
+    ---------------------------------------------------------------------------
+    TRUNCATE TABLE bronze.crm_prd_info;
+    SET @DynamicSQL = '
+    BULK INSERT bronze.crm_prd_info
+    FROM ''' + @BasePath + '\source_crm\prd_info.csv''
+    WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
+    EXEC sp_executesql @DynamicSQL;
 
-	-- For bronze.erp_cust_az12
-	TRUNCATE table bronze.erp_cust_az12;
-	SET @DynamicSQL = '
-	BULK INSERT bronze.erp_cust_az12
-	FROM ''' + @BasePath + '\source_erp\CUST_AZ12.csv''
-	WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
-	EXEC sp_executesql @DynamicSQL;
+    ---------------------------------------------------------------------------
+    -- 3. Load crm_sales_details
+    ---------------------------------------------------------------------------
+    TRUNCATE TABLE bronze.crm_sales_details;
+    SET @DynamicSQL = '
+    BULK INSERT bronze.crm_sales_details
+    FROM ''' + @BasePath + '\source_crm\sales_details.csv''
+    WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
+    EXEC sp_executesql @DynamicSQL;
 
-	-- For bronze.erp_loc_a101
-	TRUNCATE table bronze.erp_loc_a101;
-	SET @DynamicSQL = '
-	BULK INSERT bronze.erp_loc_a101
-	FROM ''' + @BasePath + '\source_erp\LOC_A101.csv''
-	WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
-	EXEC sp_executesql @DynamicSQL;
+    ---------------------------------------------------------------------------
+    -- 4. Load erp_cust_az12
+    ---------------------------------------------------------------------------
+    TRUNCATE TABLE bronze.erp_cust_az12;
+    SET @DynamicSQL = '
+    BULK INSERT bronze.erp_cust_az12
+    FROM ''' + @BasePath + '\source_erp\CUST_AZ12.csv''
+    WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
+    EXEC sp_executesql @DynamicSQL;
 
-	-- For bronze.erp_px_cat_g1v2
-	TRUNCATE table bronze.erp_px_cat_g1v2;
-	SET @DynamicSQL = '
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM ''' + @BasePath + '\source_erp\PX_CAT_G1V2.csv''
-	WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
-	EXEC sp_executesql @DynamicSQL;
+    ---------------------------------------------------------------------------
+    -- 5. Load erp_loc_a101
+    ---------------------------------------------------------------------------
+    TRUNCATE TABLE bronze.erp_loc_a101;
+    SET @DynamicSQL = '
+    BULK INSERT bronze.erp_loc_a101
+    FROM ''' + @BasePath + '\source_erp\LOC_A101.csv''
+    WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
+    EXEC sp_executesql @DynamicSQL;
+
+    ---------------------------------------------------------------------------
+    -- 6. Load erp_px_cat_g1v2
+    ---------------------------------------------------------------------------
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+    SET @DynamicSQL = '
+    BULK INSERT bronze.erp_px_cat_g1v2
+    FROM ''' + @BasePath + '\source_erp\PX_CAT_G1V2.csv''
+    WITH ( FIRSTROW = 2, FIELDTERMINATOR = '','', TABLOCK );';
+    EXEC sp_executesql @DynamicSQL;
+
 END
